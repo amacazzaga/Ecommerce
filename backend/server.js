6913b32c787eea9ab55503c2cce04bd4f8851059
unknown <ag.macazz@gmail.com>
2022-08-16 15:04:49 -0300

    user route, get

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 require (`dotenv`).config()
 const express = require(`express`)
 const mongoose = require(`mongoose`)
+const userRoutes = require(`./routes/user`)
 
 // express app//
 const app = express();//settting the app to express
+//middleware//
+/*Las funciones de middleware son funciones que tienen acceso al objeto
+de solicitud (req), y al objeto de respuesta (res)*/
+app.use(express.json())
+app.use(`/`,userRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
