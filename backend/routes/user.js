8ea5aa7407e55bfd2190bf51a18556962a1ec848
unknown <ag.macazz@gmail.com>
2022-08-16 15:52:32 -0300

    user schema, email type & required

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,4 +5,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.json(`user , get`);
 });
+router.post("/", (req, res) => {
+    res.json(`user , post`);
+  });
 module.exports = router;
