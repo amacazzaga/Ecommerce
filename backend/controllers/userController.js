47acbaabe9cd751b2f245d0cb7191fe79e1e223e
unknown <ag.macazz@gmail.com>
2022-08-16 16:06:14 -0300

    userControllers folder

---
+++ backend/controllers/userController.js
@@ -0,0 +1,15 @@
+const Users = require(`..models/userModels`);
+const mongoose = require(`mongoose`);
+
+const getUsers = async (req, res) => {
+  try {
+    const user = await Users.find();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ mss: "error" });
+  }
+};
+
+module.exports = {
+    getUsers
+}
