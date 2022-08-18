2c9c7e790dae71f027dc4c19e666efa71ede0b2f
unknown <ag.macazz@gmail.com>
2022-08-18 11:17:01 -0300

    deleteuser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require(`../models/userModels`);
 const mongoose = require(`mongoose`);
 
+
+//get all users//
 const getUsers = async (req, res) => {
   try {
     const user = await User.find();
@@ -9,6 +11,7 @@ const getUsers = async (req, res) => {
     res.status(400).json({ mss: "error" });
   }
 };
+//create new user//
 const createUser = async (req, res) => {
   const { email, password } = req.body; //destructuring
   try {
@@ -18,6 +21,7 @@ const createUser = async (req, res) => {
     res.status(400).json({ mss: "error" });
   }
 };
+//get a single user//
 const getUser = async (req, res) => {
   const { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -29,9 +33,22 @@ const getUser = async (req, res) => {
   }
   res.status(200).json(user);
 };
+//delete user///
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+   return res.status(404).json(`not a valid id`);
+  }
+  const user = await User.findByIdAndDelete(id);
+  if (!user) {
+   return res.status(400).json(`no such user`);
+  }
+  res.status(200).json(user);
+};
 
 module.exports = {
   getUsers,
   createUser,
   getUser,
+  deleteUser
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,15 @@
 const express = require(`express`);
-const { createUser ,getUsers,getUser} = require(`../controllers/userController`);
+const {
+  createUser,
+  getUsers,
+  getUser,
+  deleteUser,
+} = require(`../controllers/userController`);
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/", createUser);
-router.get("/:id",getUser);
+router.get("/:id", getUser);
+router.delete(`/:id`,deleteUser)
 module.exports = router;
