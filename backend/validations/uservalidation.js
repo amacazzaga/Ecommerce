4e9c5888206162e1253b9153808533ea700bb53e
unknown <ag.macazz@gmail.com>
2022-08-23 16:14:40 -0300

    uservalidate on yup

---
+++ backend/validations/uservalidation.js
@@ -0,0 +1,11 @@
+const yup = require(`yup`);
+
+const userValidate = yup.object().shape({
+  name: yup.string().min(4).max(30).required(),
+  lastName: yup.string().min(4).max(30).required(),
+  age: yup.number().positive().integer().min(18).max(99).required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(4).max(18).required(),
+});
+
+module.exports = userValidate;
