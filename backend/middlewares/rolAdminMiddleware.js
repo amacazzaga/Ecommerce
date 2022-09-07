98d057507f8947616676af3822500e3cb2454d49
unknown <ag.macazz@gmail.com>
2022-09-07 16:07:12 -0300

    indentation

--- backend/middlewares/rolAdminMiddleware.js
+++ backend/middlewares/rolAdminMiddleware.js
@@ -1,18 +1,18 @@
 const User = require(`../models/userModels`);
 const rolAdmin = async (req, res, next) => {
-    const id = req.header(`user-id`);
-    if (!id) return res.status(400).json(`no user found`);
-    try {
-    const user = await User.findById(id)
-      const isUserAdmin = user.rol[0]
-      if(isUserAdmin!="admin"){
-        return res.status(401).json(`no admin`)
-      }
-      console.log(isUserAdmin)
-     console.log(id)
-      next();
-    } catch (err) {
-      return res.status(401).json(`invalid token`);
+  const id = req.header(`user-id`);
+  if (!id) return res.status(400).json(`no id on header found`);
+  try {
+    const user = await User.findById(id);
+    const isUserAdmin = user.rol[0];
+    if (isUserAdmin != "admin") {
+      return res.status(401).json(`youre not an admin`);
     }
-  };
-  module.exports = {rolAdmin };
+    console.log(isUserAdmin);
+    console.log(id);
+    next();
+  } catch (err) {
+    return res.status(401).json(`invalid user id`);
+  }
+};
+module.exports = { rolAdmin };
