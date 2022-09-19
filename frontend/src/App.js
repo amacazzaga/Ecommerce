f1a692faf0f58604748cfd2738f3ce0545def0d7
unknown <ag.macazz@gmail.com>
2022-09-19 09:27:55 -0300

    states for email & pass field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import LoginForm from "./components/LoginForm";
 function App() {
   return (
--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 
 const LoginForm = () => {
+  const [email,setEmail]=useState("")
+  const [password,setPassword]= useState("")
   return (
     <form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e)=>setEmail(e.target.value)}></input>
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"></input>
+    <input type="password" class="form-control" id="exampleInputPassword1" onChange={(e)=>setPassword(e.target.value)}></input>
   </div>
   <div class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <label class="form-check-label" for="exampleCheck1">Remember me</label>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
