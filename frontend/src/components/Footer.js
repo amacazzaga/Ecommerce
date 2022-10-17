f373e08da4acbe0cccec543fdd1517a166db999e
unknown <ag.macazz@gmail.com>
2022-10-17 15:42:30 -0300

    use just one footer tag

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <div>
-      <footer class="footer text-center text-white ">      
-        <div >
-          © 2022 Copyright
-        </div>
-      </footer>
-    </div>
-  );
+  return <div class="footer text-center text-white ">© 2022 Copyright</div>;
 };
 
 export default Footer;
