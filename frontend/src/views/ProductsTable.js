9a5c13fa8c24663375c387acaccec7fc6282e013
unknown <ag.macazz@gmail.com>
2022-10-21 11:45:03 -0300

    table adm looking good with css col

--- frontend/src/views/ProductsTable.js
+++ frontend/src/views/ProductsTable.js
@@ -2,12 +2,15 @@ import React from "react";
 
 const ProductsTable = ({ name, price, description }) => {
   return (
-    <table class="table">
+    <table class="table container">
     <tbody>
       <tr>
-        <td>{name}</td>
-        <td>{price}</td>
-        <td>{description}</td>
+        <td className="col-3">{name}</td>
+        <td className="col-3">{price}</td>
+        <td className="col-3">{description}</td>
+        <td className="col-2">edit</td>
+        <td className="col-1">delete</td>
+        
       </tr>
     </tbody>
   </table>
--- frontend/src/views/TableHead.js
+++ frontend/src/views/TableHead.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const TableHead = () => {
   return (
-    <table class="table">
+    <table class="table container">
       <thead>
         <tr>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
+          <th className="col-3">First</th>
+          <th className="col-3">Last</th>
+          <th className="col-3">Handle</th>
+          <th className="col-2">Edit</th>
+          <th className="col-1">Delete</th>
         </tr>
       </thead>
     </table>
