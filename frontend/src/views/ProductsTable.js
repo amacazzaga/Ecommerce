b4b5fe06413a2fac18a5a52ab7e35f62c6d464f2
unknown <ag.macazz@gmail.com>
2022-10-25 13:28:56 -0300

    css on edit & delete button

--- frontend/src/views/ProductsTable.js
+++ frontend/src/views/ProductsTable.js
@@ -1,21 +1,28 @@
 import React from "react";
 
-const ProductsTable = ({ name, price, description,image,category,id }) => {
+const ProductsTable = ({ name, price, description, image, category, id }) => {
   return (
     <table class="table container">
-    <tbody>
-      <tr>
-        <td className="col-2">{name}</td>
-        <td className="col-2">{price}</td>
-        <td className="col-2">{description}</td>
-        <td className="col-2">{category}</td>
-        <td className="col-2">{image}</td>
-        <td className="col-1"><div>edit</div></td>
-        <td className="col-1"><div>delete</div></td>
-        
-      </tr>
-    </tbody>
-  </table>
+      <tbody>
+        <tr>
+          <td className="col-2">{name}</td>
+          <td className="col-2">{price}</td>
+          <td className="col-2">{description}</td>
+          <td className="col-2">{category}</td>
+          <td className="col-2">{image}</td>
+          <td className="col-1">
+            <button type="button" class="btn btn-success">
+              edit
+            </button>
+          </td>
+          <td className="col-1">
+            <button type="button" class="btn btn-danger">
+              delete
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
