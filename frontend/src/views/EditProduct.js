e0dcac74ad76c29f5a752e87121a44a814d0c707
unknown <ag.macazz@gmail.com>
2022-10-26 16:01:53 -0300

    async await function editProduct

--- frontend/src/views/EditProduct.js
+++ frontend/src/views/EditProduct.js
@@ -2,19 +2,22 @@ import React from "react";
 import LayoutLoggedAdm from "../components/LayoutLoggedAdm";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const EditProduct = () => {
+  const [product, setProduct] = useState({});
   const { id } = useParams();
+  const getProduct = async () => {
+    const resp = await axios.get(`http://localhost:4000/product/${id}`);
+    setProduct(resp.data);
+  };
   useEffect(() => {
-    axios.get(`http://localhost:4000/product/${id}`).then((response) => {
-      console.log(response.data);
-    });
+    getProduct();
   }, []);
 
   return (
     <LayoutLoggedAdm>
-      <div> {id} </div>
+      <div>{product.name}</div>
     </LayoutLoggedAdm>
   );
 };
