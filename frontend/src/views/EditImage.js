87c07abceaf364782501569b69a1450a8a6cb757
unknown <ag.macazz@gmail.com>
2022-11-22 17:01:41 -0300

    useEffect on editImage

--- frontend/src/views/EditImage.js
+++ frontend/src/views/EditImage.js
@@ -1,22 +1,44 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import LayoutLoggedAdm from "../components/LayoutLoggedAdm";
 
 const EditImage = () => {
   const [cookie] = useCookies();
-  const [file, setFile] = useState();
+  const { id } = useParams();
   const token = cookie.token;
-
+  const [file, setFile] = useState();
+  //
+  const getProduct = async () => {
+    const resp = await axios.get(`http://localhost:4000/product/${id}`);
+    console.log(resp)
+  };
+  ///
   const onSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("image", file);
-   const resp = await axios.post("http://localhost:4000/images", formData, {
-      headers: { "Content-Type": "multipart/form-data", Authorization: token },
-    }).then((resp)=>{console.log(resp)});
+    const resp = await axios
+      .post("http://localhost:4000/images", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: token,
+        },
+      })
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
+  //
+  useEffect(() => {
+    
+    getProduct();
+  }, []);
 
   return (
     <LayoutLoggedAdm>
