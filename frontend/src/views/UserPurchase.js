1c28c0306f1e2a56a1673d67ee300c510d0e1e17
unknown <ag.macazz@gmail.com>
2022-11-25 12:58:46 -0300

    getUser from UserPurchase

--- frontend/src/views/UserPurchase.js
+++ frontend/src/views/UserPurchase.js
@@ -3,28 +3,48 @@ import LayoutLoggedAdm from "../components/LayoutLoggedAdm";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import { useEffect} from "react";
+import { useEffect, useState } from "react";
 
 const UserPurchase = () => {
-    const [cookie] = useCookies();
-    const token = cookie.token;
-    const {id} =useParams()
-    const getSales = async () => {
-        const resp = await axios
-          .get(`http://localhost:4000/sales/${id}`, {
-            headers: { Authorization: token },
-          })
-          .then((resp) => {
-           console.log(resp.data)
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
-      useEffect(() => {
-       getSales()
-      }, [])
-      
+  const [cookie] = useCookies();
+  const [user,setUser]=useState()
+  const [sales, setSales] = useState();
+  const token = cookie.token;
+  const { id } = useParams();
+
+  const getUser = async () => {
+    const resp = await axios
+      .get(`http://localhost:4000/user/${id}`, {
+        headers: { Authorization: token },
+      })
+      .then((resp) => {
+        setUser(resp.data);
+        console.log(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  //
+  const getSales = async () => {
+    const resp = await axios
+      .get(`http://localhost:4000/sales/${id}`, {
+        headers: { Authorization: token },
+      })
+      .then((resp) => {
+        setSales(resp.data);
+        console.log(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getUser();
+    getSales();
+  }, []);
+
   return (
     <LayoutLoggedAdm>
       <div>{id}</div>
