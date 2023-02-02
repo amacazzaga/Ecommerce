3754cc63914ac78d8399548d00c0746fe67e98de
unknown <ag.macazz@gmail.com>
2023-02-02 14:46:17 -0300

    InspectProduct with carrousel

--- frontend/src/views/InspectProduct.js
+++ frontend/src/views/InspectProduct.js
@@ -30,36 +30,40 @@ const InspectProduct = () => {
       <div>
         <div
           id="carouselExampleControls"
-          class="carousel slide"
+          class="carousel slide bg-grey"
           data-bs-ride="carousel"
         >
           <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img
-                src={
-                  product ? cloudFrontBaseUrl + product.imageNameArray[0] : ""
-                }
-                class="d-block w-100"
-                alt="..."
-              />
+            <div class="carousel-item active ">
+              <div className="container d-flex col-6">
+                <img
+                  src={
+                    product ? cloudFrontBaseUrl + product.imageNameArray[0] : ""
+                  }
+                  class="d-block w-100 m-3"
+                  alt="..."
+                />
+              </div>
             </div>
             <div class="carousel-item">
-              <img
-                src={
-                  product ? cloudFrontBaseUrl + product.imageNameArray[1] : ""
-                }
-                class="d-block w-100"
-                alt="..."
-              />
+              <div className="container d-flex col-6">
+                <img
+                  src={
+                    product ? cloudFrontBaseUrl + product.imageNameArray[1] : ""
+                  }
+                  class="d-block w-100 m-3"
+                  alt="..."
+                />
+              </div>
             </div>
           </div>
           <button
             class="carousel-control-prev"
-            type="button"
+            type="button "
             data-bs-target="#carouselExampleControls"
             data-bs-slide="prev"
           >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="carousel-control-prev-icon bg-dark rounded-circle" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
           </button>
           <button
@@ -68,7 +72,7 @@ const InspectProduct = () => {
             data-bs-target="#carouselExampleControls"
             data-bs-slide="next"
           >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="carousel-control-next-icon bg-dark rounded-circle" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
         </div>
