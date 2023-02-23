95b58b9ce4616f07a9414b83ede5db9551fa1b72
unknown <ag.macazz@gmail.com>
2023-02-23 01:14:56 -0300

    CartProduct responsive

--- frontend/src/views/CartProduct.js
+++ frontend/src/views/CartProduct.js
@@ -29,12 +29,11 @@ const CartProduct = ({
     });
     return foundItem.amount;
   };
-//
   //
   useEffect(() => {
     reloadProducts();
   }, []);
-
+  //
   return (
     <div className="container">
       <div class="card  border border-2 m-3">
@@ -47,12 +46,12 @@ const CartProduct = ({
           <h5 class="card-title">{name}</h5>
           <p class="card-text">{description}</p>
         </div>
-        <div className="container d-flex justify-content-between ">
-          <div className="d-flex  ps-3 fw-bolder">{"$" + price}</div>
+        <div className="container d-flex justify-content-between align-items-center ">
+          <div className=" ps-3 fw-bolder"><div>{"$" + price}</div></div>
           <input
+            className="fw-bold form-control w-50"
             type="number"
             min="1"
-            class="w-25"
             onChange={(e) => {
               const fromLocaleStorage = localStorage.getItem("product");
               const newAmounts = JSON.parse(fromLocaleStorage);
@@ -65,11 +64,11 @@ const CartProduct = ({
             }}
             value={amountInLocaleStorage()}
             placeholder={amountInLocaleStorage()}
-          />
+          ></input>
         </div>
         <div>
           <button
-            className="btn btn-danger ms-3 m-1 fw-bolder "
+            className="btn btn-danger ms-1rem m-1 fw-bolder "
             onClick={deleteItem}
           >
             Delete
