403415dc90a1a7e8a8d3c005f3e23919e9e85239
unknown <ag.macazz@gmail.com>
2023-04-19 14:38:18 -0300

    https on localhost

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,30 @@
 require(`dotenv`).config();
 const express = require(`express`);
-const cors = require(`cors`)
+const cors = require(`cors`);
 const mongoose = require(`mongoose`);
+const fs = require('fs');
+const https = require("https");
 const userRoutes = require(`./routes/users`);
 const productRoutes = require(`./routes/products`);
 const salesRoutes = require(`./routes/sales`);
-const imagesRoutes = require(`./routes/images`)
+const imagesRoutes = require(`./routes/images`);
+
 // express app//
+
 const app = express(); //setting the app to express
-app.use(cors())
+const privateKey  = fs.readFileSync('./sslcert/server.key');
+const certificate = fs.readFileSync('./sslcert/server.crt');
+const credentials = {key: privateKey, cert: certificate};
+const httpsServer = https.createServer(credentials,app);
+
+app.use(cors());
 //middleware//
 /*Las funciones de middleware son funciones que tienen acceso al objeto
 de solicitud (req), y al objeto de respuesta (res)*/
 app.use(express.json());
 app.use(`/`, (req, res, next) => {
   console.log(req.method);
+  console.log(certificate)
   next();
 });
 //user routes//
@@ -24,13 +34,13 @@ app.use(`/product`, productRoutes);
 //sales routes//
 app.use(`/sales`, salesRoutes);
 //images routes//
-app.use(`/images`,imagesRoutes)
+app.use(`/images`, imagesRoutes);
 //mongoose connect//
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     //once in mongoose//
-    app.listen(process.env.PORT, () => {
+    httpsServer.listen(process.env.PORT, () => {
       //port 4000
       console.log("connected to db & listening on", process.env.PORT);
     });
